b757eed39a0355797c6b49d4d5c6bce210bcef4b
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2025-09-27 16:06:36 +0100

    refactor: remove unused DialogTrigger import in MarketDetail component

--- frontend/src/components/Markets/MarketDetail.tsx
+++ frontend/src/components/Markets/MarketDetail.tsx
@@ -12,7 +12,6 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
